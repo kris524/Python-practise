9ce77a55cc0ebad9ce97b44baac65980f0f195fc
Kris <kristian_dilov@abv.bg>
2022-09-05 23:21:38 +0300

    initial commit

--- solutions/BST.py
+++ solutions/BST.py
@@ -1,44 +1,33 @@
-# Build a BST
-class BST:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root: return None
+        
+            
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+            
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+            
+        elif root.val == key:  
+            #Here we return the value that will overwrite the soon to be deleted key
+            if root.left == None and root.right == None: return None
+            elif root.left == None and root.right != None: return root.right
+            elif root.left != None and root.right == None: return root.left
 
-    def insert(self, value):
-        if self.value > value:
-            if self.left is None:
-                self.left = BST(value)
-            else:
-                self.left.insert(value)
-        else:
-            if self.right is None:
-                self.right = BST(value)
-
-            else:
-                self.right.insert(value)
-
-        return self
-
-    def contains(self, value):
-        # if self.value == value:
-        #     return True
-        if self.value > value:
-
-            if self.left is None:
-                return False
-            else:
-                self.left.contains(value)
-        elif self.value < value:
-            if self.right is None:
-                return False
-            else:
-                self.right.contains(value)
-        else:
-            return True
-        # Write your code here.
-
-    def remove(self, value):
-        # Write your code here.
-        # Do not edit the return statement of this method.
-        return self
+            #if both
+            pointer = root.right
+            #while a left branch exists, move all the way to the most left node 
+            while pointer.left: pointer = pointer.left
+            #we leave and overwrite the root by that most left value on the right branch
+            root.val = pointer.val
+            root.right = self.deleteNode(root.right, root.val)
+            
+            
+        return root
